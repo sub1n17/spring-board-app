0cc581e59aaf1def7235310fc03d05f69e70be70
sub1n17 <sub1n17@naver.com>
2026-06-29 17:13:34 +0900

    commit 0.0.1

--- src/main/java/kr/co/sboard/dto/AppInfoDTO.java
+++ src/main/java/kr/co/sboard/dto/AppInfoDTO.java
@@ -1,6 +1,5 @@
 package kr.co.sboard.dto;
 
-
 import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,10 +9,9 @@ import org.springframework.stereotype.Component;
 public class AppInfoDTO {
 
     @Value("${spring.application.name}")
-    private  String appName;
+    private String appName;
 
     @Value("${spring.application.version}")
-    private  String appVersion;
-
+    private String appVersion;
 
 }
